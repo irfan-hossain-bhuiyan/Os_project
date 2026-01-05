38795b0ece018008d32510f6c6260a9338524441
Cyb3r-Bishop <ariffoysalruetcse21@gmail.com>
2026-01-05 18:34:56 +0600

    stack

--- stack.c
+++ stack.c
@@ -7,22 +7,29 @@
 static uint8_t stack_pool[MAX_STACKS][STACK_SIZE] __attribute__((aligned(16)));
 static uint8_t stack_used[MAX_STACKS] = {0};
 
-void* alloc_stack(size_t size) {
-    //if (size > STACK_SIZE) return NULL;
-    for (int i = 0; i < MAX_STACKS; ++i) {
-        if (!stack_used[i]) {
+void *alloc_stack(size_t size)
+{
+    // if (size > STACK_SIZE) return NULL;
+    for (int i = 0; i < MAX_STACKS; ++i)
+    {
+        if (!stack_used[i])
+        {
             stack_used[i] = 1;
-            return (void*)(stack_pool[i]); 
+            return (void *)(stack_pool[i]);
         }
     }
     klog_error("[ERROR] alloc_stack: no free stacks available\n");
     return NULL;
 }
 
-int free_stack(void* ptr) {
-    for (int i = 0; i < MAX_STACKS; ++i) {
-        if ((void*)stack_pool[i] == ptr) {
-            if (!stack_used[i]) {
+int free_stack(void *ptr)
+{
+    for (int i = 0; i < MAX_STACKS; ++i)
+    {
+        if ((void *)stack_pool[i] == ptr)
+        {
+            if (!stack_used[i])
+            {
                 klog_error("free_stack: stack already free\n");
                 return -1; // Already free
             }
@@ -34,10 +41,13 @@ int free_stack(void* ptr) {
     return -2; // Not found
 }
 
-int get_stack_usage(void) {
+int get_stack_usage(void)
+{
     int count = 0;
-    for (int i = 0; i < MAX_STACKS; i++) {
-        if (stack_used[i]) {
+    for (int i = 0; i < MAX_STACKS; i++)
+    {
+        if (stack_used[i])
+        {
             count++;
         }
     }
